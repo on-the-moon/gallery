805809afd75a6f7d20867a4c832b313c412d3a14
knonuh94 <97404500+knonuh94@users.noreply.github.com>
2022-04-13 00:22:54 -0600

    created component folder and created DiceRollerForm component - updated App.css for better readability
    removed existing code in App.js and moved to a component (DiceRollerForm), added attack rolls and damage bonuses to existing form, updated existing css with new classNames and grid-areas for better readability

--- src/App.js
+++ src/App.js
@@ -1,68 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
+import DiceRollerForm from "./Components/DiceRollerForm";
+import React from "react";
 
 function App() {
-  const titleText = "Roll For Whatever";
-
-  const [diceType, setDiceType] = useState("0");
-  const [numDice, setNumDice] = useState(1);
-  const [diceTotal, setDiceTotal] = useState(0);
-  const diceTypes = [4, 6, 8, 12, 20, 50, 100];
-
-  const rollD = (dice) => Math.floor(Math.random() * dice + 1);
-
-  //uses rollD function to roll multiple dice and return a total
-  const rollMultiple = () => {
-    setDiceTotal(0);
-
-    for (let counter = numDice; counter !== 0; counter--) {
-      setDiceTotal((diceTotal) => diceTotal + rollD(diceType));
-    }
-  };
-
   return (
-    <div>
-      <h1 className="header">{titleText}</h1>
-      <div className="dice-form">
-        <form name="dice-roller-input" className="dice-roller-input">
-          <h3 className="dropdown-label type label">Select Dice Type</h3>
-          {/* diceType: {diceType} */}
-          <select
-            name="dice-dropdown"
-            className="dice-dropdown"
-            onChange={(e) => {
-              setDiceType(e.target.value);
-            }}
-            value={diceType}
-          >
-            {diceTypes.map((diceOption, i) => (
-              <option
-                name={diceOption}
-                value={diceOption}
-                key={"diceOption_" + i}
-              >
-                d{diceOption}
-              </option>
-            ))}
-          </select>
-          <h3 className="dropdown-label number label">Enter Number of Dice</h3>
-          <input
-            type="number"
-            min="1"
-            value={numDice}
-            onChange={(e) => setNumDice(e.target.value)}
-            id="count"
-            className="enter-number"
-          />
-          <button type="button" onClick={rollMultiple} className="roll-button">
-            <p>Roll It</p>
-          </button>
-          <h3 name="roll-total" className="roll-total">
-            You Rolled a Total of
-            <span id="roll-total"> {diceTotal}</span>
-          </h3>
-        </form>
-      </div>
+    <div className="App">
+      <DiceRollerForm />
     </div>
   );
 }
---
+++ src/Components/DiceRollerForm.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+function DiceRollerForm() {
+  const titleText = "Roll For Whatever";
+
+  const [diceType, setDiceType] = useState("4");
+  const [numDice, setNumDice] = useState(1);
+  const [diceTotal, setDiceTotal] = useState(0);
+  const [attackBonus, setAttackBonus] = useState(0);
+  const [damageBonus, setDamageBonus] = useState(0);
+  const [attackRollTotal, setAttackRollTotal] = useState(0);
+  const diceTypes = [4, 6, 8, 12, 20, 50, 100];
+
+  const rollD = (dice) => Math.floor(Math.random() * dice + 1);
+
+  //uses rollD function to roll multiple dice and return a total
+  const rollMultiple = () => {
+    setDiceTotal(attackBonus);
+    setAttackRollTotal(rollD(20) + attackBonus);
+    for (let counter = numDice; counter !== 0; counter--) {
+      setDiceTotal((diceTotal) => diceTotal + rollD(diceType));
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="header">{titleText}</h1>
+      <div className="dice-form">
+        <form name="dice-roller-input" className="dice-roller-input">
+          <h2 className="attackBonus-label number label grid-section">
+            Enter Your Attack Bonus
+          </h2>
+          <input
+            type="number"
+            min="0"
+            value={attackBonus}
+            onChange={(e) => setAttackBonus(+e.target.value)}
+            id="count"
+            className="attackBonus-entry grid-section"
+          />
+          <h2 className="diceType-label type label grid-section">
+            Select Dice Type
+          </h2>
+          <select
+            name="dice-dropdown"
+            className="diceType-entry grid-section"
+            onChange={(e) => {
+              setDiceType(+e.target.value);
+            }}
+            value={diceType}
+          >
+            {diceTypes.map((diceOption, i) => (
+              <option
+                name={diceOption}
+                value={diceOption}
+                key={"diceOption_" + i}
+              >
+                d{diceOption}
+              </option>
+            ))}
+          </select>
+          <h2 className="numDice-label number label grid-section">
+            Enter Number of Dice
+          </h2>
+          <input
+            type="number"
+            min="1"
+            value={numDice}
+            onChange={(e) => setNumDice(+e.target.value)}
+            id="count"
+            className="numDice-entry grid-section"
+          />
+          <h2 className="damageBonus-label grid-section">
+            Enter your damage bonus
+          </h2>
+          <input
+            type="number"
+            min="0"
+            value={damageBonus}
+            onChange={(e) => setDamageBonus(+e.target.value)}
+            id="bonus"
+            className="damageBonus-entry grid-section"
+          />
+          <button
+            type="button"
+            onClick={rollMultiple}
+            className="roll-button grid-section"
+          >
+            <p>Roll It</p>
+          </button>
+          <h2
+            name="attack-roll-total"
+            className="attackRoll-total grid-section"
+          >
+            {" "}
+            Rolled
+            <span> {attackRollTotal}</span> to hit.
+          </h2>
+          <h2
+            name="damage-roll-total"
+            className="damageRoll-total grid-section"
+          >
+            <span id="damage-roll-total">{diceTotal}</span> damage dealt on hit!
+          </h2>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default DiceRollerForm;
