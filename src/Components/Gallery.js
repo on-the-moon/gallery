d6561dbad45fda35c8e8115d500f7751e85acfda
knonuh94 <97404500+knonuh94@users.noreply.github.com>
2022-05-06 20:32:58 -0600

    added image enlarging to gallery component

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
 import album from "./api-mock.json";
+import styles from "./Gallery.module.css";
 function Gallery() {
-  const [imageList, setImageList] = useState(album.data.images);
-  console.log("album", album);
-  console.log("imageList", imageList);
+  const imageList = album.data.images;
+  const [openPopup, setOpenPopup] = useState(false);
+  const [currentImage, setCurrentImage] = useState();
+  const togglePopup = (e) => {
+    setOpenPopup(!openPopup);
+    setCurrentImage(e.target.src);
+  };
 
   return (
-    <div className="image-grid">
-      {imageList.map((album) => (
-        <img src={album.link} key={album.id} alt={album.id}></img>
-      ))}
+    <div className={styles.gallery_block}>
+      <div className={styles.image_grid}>
+        {imageList.map((image) => (
+          <img
+            src={image.link}
+            key={image.id}
+            alt={image.description}
+            className={styles.images}
+            onClick={togglePopup}
+          />
+        ))}
+      </div>
+      {openPopup && (
+        <div
+          className={styles.image_popup_box}
+          onClick={() => setOpenPopup(!openPopup)}
+        >
+          <img
+            src={currentImage}
+            className={styles.image_popup}
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
     </div>
   );
 }
